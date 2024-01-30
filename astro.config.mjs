7ec62ae8c87e7e0bdd0a7c14b79ee1b4684bdb18
stdpi <stdpi@proton.me>
2024-01-30 19:24:54 +0700

    fix: toc autogen real

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
       sidebar: [
         {
           label: "Hướng dẫn",
-					autogenerate: { directory: "reference" },
+					autogenerate: { directory: "guides" },
         },
       ],
     }),
